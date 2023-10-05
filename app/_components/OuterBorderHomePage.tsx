59eefa6c0aff0ea951524b75680fa06126e62754
LazarevicV <vukadinlazarevic@gmail.com>
2023-10-06 01:47:27 +0200

    outer border used on homepage

---
+++ app/_components/OuterBorderHomePage.tsx
@@ -0,0 +1,10 @@
+// @ts-ignore
+const OuterBorderHomePage = ({children}) => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      {children}
+    </div>
+  )
+}
+
+export default OuterBorderHomePage
