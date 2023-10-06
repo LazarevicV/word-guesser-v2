6e179a4703a1860bd56b9f461d81534fc33c98f6
LazarevicV <vukadinlazarevic@gmail.com>
2023-10-06 02:56:42 +0200

    component which shows game instructions

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import OuterBorderHomePage from "@/app/_components/OuterBorderHomePage";
 import HomePageTitle from "@/app/_components/HomePageTitle";
 import HomePageParagraph from "@/app/_components/HomePageParagraph";
 import StartGameDiv from "@/app/_components/StartGameDiv";
+import HomePageLogo from "@/app/_components/HomePageLogo";
 
 export default function Home() {
 
@@ -11,12 +12,16 @@ export default function Home() {
   const paragraphMessage = 'Created by Vule and Ana'
   const startPath = '/start'
   const messageStart = 'Start a new game'
+  const imagePath = '/homepageImage.png'
 
   return (
-    <OuterBorderHomePage>
-      <HomePageTitle message={title}/>
-      <HomePageParagraph message={paragraphMessage}/>
-      <StartGameDiv path={startPath} message={messageStart}/>
-    </OuterBorderHomePage>
+    <>
+      <OuterBorderHomePage>
+        <HomePageTitle message={title}/>
+        <HomePageParagraph message={paragraphMessage}/>
+        <HomePageLogo imagePath={imagePath}/>
+        <StartGameDiv path={startPath} message={messageStart}/>
+      </OuterBorderHomePage>
+    </>
   );
 }
